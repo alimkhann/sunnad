80d695fb93e17eba2991eb0b2b553225d426329c
alim <74613123+alimkhann@users.noreply.github.com>
2026-03-02 19:53:14 +0500

    android: update localized strings and groups rpc tests

--- sunnad-android/app/src/test/java/com/arystan/almasuly/sunnadandroid/data/remote/repository/SupabaseGroupsRepositoryTest.kt
+++ sunnad-android/app/src/test/java/com/arystan/almasuly/sunnadandroid/data/remote/repository/SupabaseGroupsRepositoryTest.kt
@@ -2,6 +2,7 @@ package com.arystan.almasuly.sunnadandroid.data.remote.repository
 
 import com.arystan.almasuly.sunnadandroid.core.model.GroupNudgeStatus
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Test
 
 class SupabaseGroupsRepositoryTest {
@@ -11,5 +12,18 @@ class SupabaseGroupsRepositoryTest {
         assertEquals(GroupNudgeStatus.DUPLICATE, SupabaseGroupsRepository.mapNudgeStatus("duplicate"))
         assertEquals(GroupNudgeStatus.FORBIDDEN, SupabaseGroupsRepository.mapNudgeStatus("forbidden"))
         assertEquals(GroupNudgeStatus.ERROR, SupabaseGroupsRepository.mapNudgeStatus("unexpected"))
+    }
+
+    @Test
+    fun parseUuidFromRpcResponseSupportsScalarObjectAndArrayShapes() {
+        val scalar = SupabaseGroupsRepository.parseUuidFromRpcResponse("\"f91c03bf-cd81-40be-ad07-56f0128e19a2\"")
+        val objectPayload = SupabaseGroupsRepository.parseUuidFromRpcResponse("{\"id\":\"f91c03bf-cd81-40be-ad07-56f0128e19a2\"}")
+        val arrayPayload = SupabaseGroupsRepository.parseUuidFromRpcResponse("[{\"group_id\":\"f91c03bf-cd81-40be-ad07-56f0128e19a2\"}]")
+
+        assertNotNull(scalar)
+        assertNotNull(objectPayload)
+        assertNotNull(arrayPayload)
+        assertEquals(scalar, objectPayload)
+        assertEquals(objectPayload, arrayPayload)
     }
 }
